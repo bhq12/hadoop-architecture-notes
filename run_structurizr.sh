27d7ffbe07972d4c95cc920267c82783526f0ad9
bhq12 <bhq12@uclive.ac.nz>
2024-04-01 18:56:29 +1000

    First pass at trying to initialise a structurizr project - tired, dinner time. Will get working later

---
+++ run_structurizr.sh
@@ -0,0 +1,3 @@
+# NOTE: This one doesn't actually work 
+# need to do something abour the STRUCTURIZR_WORKSPACE_PATH or something
+docker run -it --rm -p 8080:8080 -v PATH:/home/brook/Documents/repos/hadoop-architecture-notes structurizr/lite
